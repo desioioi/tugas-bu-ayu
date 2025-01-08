77467399430d94807e1c7a70e3c88a823c456398
desioioi <desidwifatmawati85@gmail.com>
2025-01-08 11:13:48 +0700

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,37 @@
+// variabel dengan let
+let nama = "Desi dan Desy";
+
+//tampilan data
+console.log(nama);
+
+//kita ubah nilai variabel nama
+let tersangka = "Agus Buntung";
+console.log(tersangka)
+
+//2. variabel dengan var
+var namaDepan = "Gus";
+console.log(namaDepan);
+namaDepan= "Maulana";
+console.log(namaDepan);
+
+//bedanya dengan let
+let jajan = "Seblak";
+{
+    let jajan = "Ayam Geprek";
+    console.log(jajan);
+}
+console.log(jajan);
+
+//jika dengan var
+let traktir = "Bakso Mercon";
+{
+    let traktir = "es teh jumbo";
+    console.log(traktir);
+}
+console.log(jajan);
+
+//variabel dengan const
+conts TTL = "1 Januari 2025";
+console.log(TTL);
+//const TTL = "1 Pembruari 2025"
+//ini tidak boelh dilakukan karena cont nilainya tetap
